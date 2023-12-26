e187050a43eb9b935a483020fc3130174ce5489d
Syed Ali Mansoor <alimansoor4812@gmail.com>
2023-12-26 19:48:48 +0800

    feat: create some game functions for state initialization

---
+++ src/domain/functions.ts
@@ -0,0 +1,96 @@
+import { GRID_SIZE } from "./constants";
+import {
+  Fleet,
+  Game,
+  GamePhase,
+  Grid,
+  ShipCategories,
+  ShipOrientation,
+} from "./game";
+
+export function initializeGame(): Game {
+  return {
+    turn: 0,
+    phase: GamePhase.Planning,
+    winner: null,
+
+    players: [
+      {
+        name: "",
+        isHuman: true,
+        hits: 0,
+        misses: 0,
+        grid: initializeGrid(),
+        fleet: initializeFleet(),
+      },
+      {
+        name: "Computer",
+        isHuman: false,
+        hits: 0,
+        misses: 0,
+        grid: initializeGrid(),
+        fleet: initializeFleet(),
+      },
+    ],
+  };
+}
+
+function initializeGrid(): Grid {
+  return Array.from({ length: GRID_SIZE }, (_, row) =>
+    Array.from({ length: GRID_SIZE }, (_, column) => ({
+      location: { row, column },
+      ship: null,
+      isHit: false,
+    }))
+  );
+}
+
+function initializeFleet(): Fleet {
+  return [
+    {
+      category: ShipCategories.Carrier,
+      name: "carrier",
+      length: 5,
+      isPlaced: false,
+      isPositionValid: false,
+      orientation: ShipOrientation.Horizontal,
+      squares: [],
+    },
+    {
+      category: ShipCategories.Battleship,
+      name: "battleship",
+      length: 4,
+      isPlaced: false,
+      isPositionValid: false,
+      orientation: ShipOrientation.Horizontal,
+      squares: [],
+    },
+    {
+      category: ShipCategories.Destroyer,
+      name: "destroyer",
+      length: 3,
+      isPlaced: false,
+      isPositionValid: false,
+      orientation: ShipOrientation.Horizontal,
+      squares: [],
+    },
+    {
+      category: ShipCategories.Submarine,
+      name: "submarine",
+      length: 3,
+      isPlaced: false,
+      isPositionValid: false,
+      orientation: ShipOrientation.Horizontal,
+      squares: [],
+    },
+    {
+      category: ShipCategories.PatrolBoat,
+      name: "patrol boat",
+      length: 2,
+      isPlaced: false,
+      isPositionValid: false,
+      orientation: ShipOrientation.Horizontal,
+      squares: [],
+    },
+  ];
+}
