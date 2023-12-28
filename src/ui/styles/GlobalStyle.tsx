e166c49a4e7440e6dcfc350b88edd72870fc5362
Syed Ali Mansoor <alimansoor4812@gmail.com>
2023-12-28 21:13:41 +0800

    style: create global styles

--- src/ui/styles/GlobalStyle.tsx
+++ src/ui/styles/GlobalStyle.tsx
@@ -2,7 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: ${(props) => props.theme.color.blue5}
+    background-color: ${(props) => props.theme.color.blue5};
+    font-family: ${(props) => props.theme.font.family.primary};
+    color: ${(props) => props.theme.color.yellow5}
+  }
+  
+  :root {
+    font-size: ${(props) => props.theme.font.size.base};
+  }
+
+  html, body, #root {
+    height: 100%;
   }
 `;
 
