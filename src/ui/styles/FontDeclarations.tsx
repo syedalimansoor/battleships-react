0c13d6d94ddb15f1c7929b41f059631dbc813c21
Syed Ali Mansoor <alimansoor4812@gmail.com>
2023-12-28 20:57:17 +0800

    fix: use absolute url for font declarations

--- src/ui/styles/FontDeclarations.tsx
+++ src/ui/styles/FontDeclarations.tsx
@@ -3,18 +3,18 @@ import { createGlobalStyle } from "styled-components";
 const FontDeclarations = createGlobalStyle`
   @font-face {
     font-family: "ChunkFive";
-    src: local("ChunkFive"), url(../fonts/ChunkFive-Regular.otf) format("opentype");
+    src: local("ChunkFive"), url(src/ui/fonts/ChunkFive-Regular.otf) format("opentype");
   }
 
   @font-face {
     font-family: "Kollektif";
-    src: local("Kollektif"), url(../fonts/Kollektif.ttf) format("truetype");
+    src: local("Kollektif"), url(src/ui/fonts/Kollektif.ttf) format("truetype");
   }
 
   @font-face {
     font-family: "Kollektif";
     font-weight: bold;
-    src: local("Kollektif"), url(../fonts/Kollektif-Bold.ttf) format("truetype");
+    src: local("Kollektif"), url(src/ui/fonts/Kollektif-Bold.ttf) format("truetype");
   }
 `;
 
