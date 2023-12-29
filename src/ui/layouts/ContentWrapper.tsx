0061f3fc8c9071dd9332851f26d40b01215f9a55
Syed Ali Mansoor <alimansoor4812@gmail.com>
2023-12-29 16:29:29 +0800

    style: create content wrapper for max-width

---
+++ src/ui/layouts/ContentWrapper.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export default function ContentWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <OuterWrapper>
+      <InnerWrapper>{children}</InnerWrapper>
+    </OuterWrapper>
+  );
+}
+
+const OuterWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
+`;
+
+const InnerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 400px;
+`;
