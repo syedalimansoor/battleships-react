b37ec25876773f01c1ced12659d26cd6feb84e03
Syed Ali Mansoor <alimansoor4812@gmail.com>
2024-01-03 20:26:34 +0800

    feat: create heading and subheading

---
+++ src/ui/components/common/Heading.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Heading = styled.h2`
+  font-size: 1.25rem;
+`;
+
+export default Heading;
--- src/ui/pages/Planning.tsx
+++ src/ui/pages/Planning.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 import ContentWrapper from "ui/layouts/ContentWrapper";
 import Grid from "../components/Grid";
+import Heading from "../components/common/Heading";
+import Subheading from "../components/common/Subheading";
 
 export default function Planning() {
   return (
     <ContentWrapper>
       <PageWrapper>
         <Grid idx={0} />
-        <p>bye</p>
+        <Heading>Place your ships</Heading>
+        <Subheading>Tap on a ship to select it.</Subheading>
       </PageWrapper>
     </ContentWrapper>
   );
