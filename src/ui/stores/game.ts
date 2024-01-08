d1abc46435ef8d5755a6d60fc6e4ad6e67af924a
Syed Ali Mansoor <alimansoor4812@gmail.com>
2024-01-08 19:35:27 +0800

    feat: create useShip

--- src/ui/stores/game.ts
+++ src/ui/stores/game.ts
@@ -1,7 +1,7 @@
 import { useStore } from "@nanostores/react";
 import { deepMap } from "nanostores";
 import { initializeGame } from "src/domain/functions";
-import { Game, PlayerIndex } from "src/domain/game";
+import { Game, PlayerIndex, Ship } from "src/domain/game";
 
 const $game = deepMap<Game>(initializeGame());
 export default $game;
@@ -26,4 +26,15 @@ export function useFleet(idx: PlayerIndex) {
   const game = useStore($game, { keys: [`players[${idx}].fleet`] });
 
   return [game.players[idx].fleet] as const;
+}
+
+export function useShip(playerIndex: PlayerIndex, shipIndex: number) {
+  const KEY = `players[${playerIndex}].fleet[${shipIndex}]` as const;
+  const game = useStore($game, { keys: [KEY] });
+
+  const setShip = (newShip: Ship) => {
+    $game.setKey(KEY, newShip);
+  };
+
+  return [game.players[playerIndex].fleet[shipIndex], setShip] as const;
 }
