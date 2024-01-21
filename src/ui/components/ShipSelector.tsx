b0056828719c453c8fdb60d8991f0d549f5cf461
Syed Ali Mansoor <alimansoor4812@gmail.com>
2024-01-21 12:34:54 +0800

    feat: enable selection of ships from ShipSelector

--- src/ui/components/ShipSelector.tsx
+++ src/ui/components/ShipSelector.tsx
@@ -2,19 +2,30 @@ import { PlayerIndex } from "src/domain/game";
 import styled from "styled-components";
 import { getShip, useFleet } from "../stores/game";
 import UnplacedShip from "./ships/UnplacedShip";
+import $selectedShipId from "../stores/selectedShipId";
 
 type Props = {
   playerIndex: PlayerIndex;
 };
 
 export default function ShipSelector(props: Props) {
-  const [fleet] = useFleet(props.playerIndex);
+  const [fleet, setFleet] = useFleet(props.playerIndex);
+
+  const selectShip = (shipToBeSelectedId: string) => {
+    $selectedShipId.set(shipToBeSelectedId);
+    setFleet({
+      ...fleet,
+      unplacedShips: fleet.unplacedShips.filter(
+        (shipId) => shipId !== shipToBeSelectedId
+      ),
+    });
+  };
 
   return (
     <Wrapper>
       {fleet.unplacedShips.map((shipId) => {
         const ship = getShip(props.playerIndex, shipId);
-        return <UnplacedShip key={shipId} ship={ship} />;
+        return <UnplacedShip key={shipId} ship={ship} onClick={selectShip} />;
       })}
     </Wrapper>
   );
