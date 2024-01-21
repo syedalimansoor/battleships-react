aa0d25a19a9df13af48f2cfe90e836514157a256
Syed Ali Mansoor <alimansoor4812@gmail.com>
2024-01-21 12:30:11 +0800

    feat: create hook for useSelectedShipId

--- src/ui/stores/selectedShipId.ts
+++ src/ui/stores/selectedShipId.ts
@@ -1,5 +1,11 @@
+import { useStore } from "@nanostores/react";
 import { atom } from "nanostores";
 
 const $selectedShipId = atom<string | null>(null);
 
 export default $selectedShipId;
+
+export const useSelectedShipId = () => {
+  const selectedShipId = useStore($selectedShipId);
+  return [selectedShipId, $selectedShipId.set] as const;
+};
